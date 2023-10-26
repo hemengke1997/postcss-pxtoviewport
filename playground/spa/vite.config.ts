2c51ecc0375b0a342478d5ddb6732da2284a63d1
hemengke1997 <23536175@qq.com>
2023-10-26 19:16:06 +0800

    ci: deploy

--- playground/spa/vite.config.ts
+++ playground/spa/vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pxtoviewport/',
+  base: '/postcss-pxtoviewport/',
   plugins: [react()],
 })
